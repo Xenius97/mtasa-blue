722501758636b426ff5913fdfffbee31fdeee5c1
Xenius97 <freejanos@gmail.com>
2026-02-03 22:10:29 +0100

    fix

--- Shared/mods/deathmatch/logic/lua/CLuaCompatibility.h
+++ Shared/mods/deathmatch/logic/lua/CLuaCompatibility.h
@@ -22,14 +22,14 @@ enum eLuaVersion
 // Include appropriate Lua headers based on version detection
 #if defined(LUA_VERSION_NUM) && LUA_VERSION_NUM == 504
     // Lua 5.4 compatibility definitions for 5.1 APIs
-    
+
     // LUA_GLOBALSINDEX was removed in 5.2+
     // In 5.4, globals are accessed via LUA_REGISTRYINDEX with key LUA_RIDX_GLOBALS
     #define LUA_GLOBALSINDEX_COMPAT    (-10002)
-    
+
     // LUA_ENVIRONINDEX was removed in 5.2+
     #define LUA_ENVIRONINDEX_COMPAT    (-10001)
-    
+
     // Compatibility layer functions
     inline void lua_getglobal_compat(lua_State* L, const char* name, int version) {
         if (version == LUA_VERSION_54) {
@@ -38,7 +38,7 @@ enum eLuaVersion
             lua_getfield(L, LUA_GLOBALSINDEX, name);
         }
     }
-    
+
     inline void lua_setglobal_compat(lua_State* L, const char* name, int version) {
         if (version == LUA_VERSION_54) {
             lua_setglobal(L, name);
@@ -46,7 +46,7 @@ enum eLuaVersion
             lua_setfield(L, LUA_GLOBALSINDEX, name);
         }
     }
-    
+
     // lua_getuservalue/lua_setuservalue changed in 5.4 to support multiple values
     inline int lua_getuservalue_compat(lua_State* L, int idx, int version) {
         if (version == LUA_VERSION_54) {
@@ -56,7 +56,7 @@ enum eLuaVersion
             return lua_type(L, -1);
         }
     }
-    
+
     inline void lua_setuservalue_compat(lua_State* L, int idx, int version) {
         if (version == LUA_VERSION_54) {
             lua_setiuservalue(L, idx, 1);  // Set first user value
@@ -69,22 +69,24 @@ enum eLuaVersion
     // Lua 5.1 - use native defines
     #define LUA_GLOBALSINDEX_COMPAT    LUA_GLOBALSINDEX
     #define LUA_ENVIRONINDEX_COMPAT    LUA_ENVIRONINDEX
-    
+
     inline void lua_getglobal_compat(lua_State* L, const char* name, int version) {
         lua_getfield(L, LUA_GLOBALSINDEX, name);
     }
-    
+
     inline void lua_setglobal_compat(lua_State* L, const char* name, int version) {
         lua_setfield(L, LUA_GLOBALSINDEX, name);
     }
-    
+
+    // Lua 5.1 doesn't have lua_getuservalue/lua_setuservalue
+    // Use environment table instead (lua_getfenv/lua_setfenv)
     inline int lua_getuservalue_compat(lua_State* L, int idx, int version) {
-        lua_getuservalue(L, idx);
+        lua_getfenv(L, idx);
         return lua_type(L, -1);
     }
-    
+
     inline void lua_setuservalue_compat(lua_State* L, int idx, int version) {
-        lua_setuservalue(L, idx);
+        lua_setfenv(L, idx);
     }
 
 #else
@@ -94,15 +96,16 @@ enum eLuaVersion
 // Helper function to get Lua version from lua_State
 inline eLuaVersion GetLuaVersion(lua_State* L) {
     if (!L) return LUA_VERSION_UNKNOWN;
-    
-    lua_Number version = lua_version(L);
-    if (version == 501) {
-        return LUA_VERSION_51;
-    } else if (version == 504) {
+
+    // Lua 5.1 doesn't have lua_version() function
+    // Use LUA_VERSION_NUM at compile time instead
+    #if defined(LUA_VERSION_NUM) && LUA_VERSION_NUM == 504
         return LUA_VERSION_54;
-    }
-    
-    return LUA_VERSION_UNKNOWN;
+    #elif defined(LUA_VERSION_NUM) && LUA_VERSION_NUM == 501
+        return LUA_VERSION_51;
+    #else
+        return LUA_VERSION_UNKNOWN;
+    #endif
 }
 
 // Helper to convert version number to string
@@ -117,12 +120,12 @@ inline const char* GetLuaVersionString(eLuaVersion version) {
 // Helper to parse version string to enum
 inline eLuaVersion ParseLuaVersion(const char* versionStr) {
     if (!versionStr) return LUA_VERSION_51;  // Default to 5.1
-    
+
     if (strcmp(versionStr, "5.4") == 0 || strcmp(versionStr, "54") == 0) {
         return LUA_VERSION_54;
     } else if (strcmp(versionStr, "5.1") == 0 || strcmp(versionStr, "51") == 0) {
         return LUA_VERSION_51;
     }
-    
+
     return LUA_VERSION_51;  // Default to 5.1 for backward compatibility
 }
